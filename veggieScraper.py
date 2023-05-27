52ae74dfc4aa0e77f94c93fbd0bbdf558726de15
feralix <haka93@msn.com>
2023-05-27 19:42:20 +0300

    uusproov

--- veggieScraper.py
+++ veggieScraper.py
@@ -12,15 +12,18 @@ class Tomato:
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 
+tomatoes = []
+
 # create a new Chrome browser instance with the headless option
 driver = webdriver.Chrome(options=chrome_options)
 
 # BARBORA ###########################################
 # navigate to the webpage
 driver.get("https://www.barbora.ee/koogiviljad-puuviljad/koogiviljad-ja-aedviljad/tomatid-ja-kurgid")
+print(driver.title)  # print the page title
 # Identify the elements containing the product data
 products = driver.find_elements(By.CSS_SELECTOR, '.b-product--wrap2')
-tomatoes = []
+
 # Iterate over the elements and print out the data
 for product in products:
     try:
@@ -41,6 +44,8 @@ for product in products:
 # SELVER ###########################################
 # navigate to the webpage
 driver.get("https://www.selver.ee/puu-ja-koogiviljad/koogiviljad-juurviljad?product_segment=1848&page=1")
+print(driver.title)  # print the page title
+
 # # Identify the elements containing the product data
 products = driver.find_elements(By.CSS_SELECTOR, '.ProductCard')
 
@@ -54,9 +59,22 @@ for product in products:
     unit_price = float(product_unit_price.replace(',', '.').split()[0])
     tomato = Tomato(product_name, price, unit_price)
     tomatoes.append(tomato)
-    
-# print the page title
-print(driver.title)
+
+# RIMI ###########################################
+# navigate to the webpage
+driver.get("https://www.rimi.ee/epood/ee/tooted/puuviljad-koogiviljad-lilled/juurviljad-ja-koogiviljad/tomat/c/SH-12-2-14")
+
+# # Identify the elements containing the product data
+products = driver.find_elements(By.CSS_SELECTOR, '.product-grid__item')
+
+# # Iterate over the elements and print out the data
+for product in products:
+    product_name = product.find_element(By.CSS_SELECTOR, '.card__name').text
+    product_price = product.find_element(By.CSS_SELECTOR, '.price-tag').text.split('\n')  # gets the price part
+    price = float(product_price[0] + '.' + product_price[1])
+    product_unit_price = float(product.find_element(By.CSS_SELECTOR, '.card__price-per').text.split(' ')[0].replace(',', '.'))  # gets the unit price part
+    tomato = Tomato(product_name, price, product_unit_price)
+    tomatoes.append(tomato)
 
 tomatoes.sort(key=lambda x: x.unit_price)
 for i, tomato in enumerate(tomatoes):
